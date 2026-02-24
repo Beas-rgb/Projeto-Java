92996fae7a09c9599b1bfb4a7051f8c368bdf925
DaviCM <davimaciel2008@gmail.com>
2026-02-24 19:38:31 -0300

    Inicio do projeto java

---
+++ copanheiro/src/main/java/com/projeto/copanheiro/CopanheiroApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.copanheiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopanheiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopanheiroApplication.class, args);
+	}
+
+}
---
+++ copanheiro/src/test/java/com/projeto/copanheiro/CopanheiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.copanheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopanheiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
